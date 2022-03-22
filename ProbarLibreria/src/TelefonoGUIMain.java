cc76449303a09fe774782dcc6d3ebdec27982782
misael.alanis <ahangel256@gmail.cim>
2022-03-22 11:52:01 -0600

    Changed alumno to telefono

---
+++ ProbarLibreria/src/TelefonoGUIMain.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TelefonoGUIMain extends JFrame {
+
+    public static void main(String[] args) {
+        JFrame interfaz = new TelefonoGUI("Interfaz");
+        interfaz.setVisible(true);
+    }
+
+}
