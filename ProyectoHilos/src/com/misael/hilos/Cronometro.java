b3cf0d1e44c419cd6356fe750d0bf58b5578d91c
misael.alanis <ahangel256@gmail.com>
2022-05-02 17:33:26 -0500

    idk

--- ProyectoHilos/src/com/misael/hilos/Cronometro.java
+++ ProyectoHilos/src/com/misael/hilos/Cronometro.java
@@ -1,4 +1,52 @@
 package com.misael.hilos;
 
-public class Cronometro {
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Cronometro implements Runnable {
+
+    int     milisCounter;
+    boolean isPaused = false;
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+
+    public void printCronometro(){
+        Date tiempo = new Date(milisCounter);
+        System.out.println(tiempo);
+    }
+
+    public void startCronometro() {
+        while (!isPaused){
+            try {
+                Thread.sleep(10);
+                if(milisCounter >= 99){
+                    System.out.println("pasó un segundo");
+                }
+                milisCounter += 1;
+                System.out.println(milisCounter);
+                printCronometro();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void pauseCronometro() {
+
+    }
+
+    public void restartCronometro() {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("run");
+        startCronometro();
+    }
+
+    public static void main(String[] args) {
+        Cronometro hilo = new Cronometro();
+        Thread cronometro = new Thread(hilo);
+        cronometro.start();
+    }
 }
