f1425b8537d480403c8396538dc891280c43d960
misael.alanis <ahangel256@gmail.com>
2022-05-03 21:55:08 -0500

    Progreso empleados

--- ProyectoHilos/src/com/misael/hilos/InterfazEmpleados.java
+++ ProyectoHilos/src/com/misael/hilos/InterfazEmpleados.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class InterfazEmpleados extends JFrame {
 
@@ -19,6 +20,7 @@ public class InterfazEmpleados extends JFrame {
     private ImageIcon  eliot_Idle;
     private ImageIcon  eliot_Work1;
     private ImageIcon  eliot_Work2;
+    private Random     random;
 
     public InterfazEmpleados() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -40,15 +42,15 @@ public class InterfazEmpleados extends JFrame {
 
             BufferedImage EliotIdle_Image  = ImageIO.read(new File("ProyectoHilos/src/resources/eliot_idle.png"));
             Image         EliotWork1_Image = new ImageIcon("ProyectoHilos/src/resources/eliott_work.gif").getImage();
-            Image         EliotWork2_Image = new ImageIcon("ProyectoHilos/src/resources/eliott_work2.gif").getImage();
+            Image         EliotWork2_Image = new ImageIcon("ProyectoHilos/src/resources/eliot_work2.gif").getImage();
 
             clint_Idle  = new ImageIcon(ClintIdle_Image);
             clint_Work1 = new ImageIcon(ClintWork1_Image);
             clint_Work2 = new ImageIcon(ClintWork2_Image);
 
-            eliot_Idle = new ImageIcon(EliotIdle_Image);
-            eliot_Idle = new ImageIcon(EliotWork1_Image);
-            eliot_Idle = new ImageIcon(EliotWork2_Image);
+            eliot_Idle  = new ImageIcon(EliotIdle_Image);
+            eliot_Work1 = new ImageIcon(EliotWork1_Image);
+            eliot_Work2 = new ImageIcon(EliotWork2_Image);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,6 +58,7 @@ public class InterfazEmpleados extends JFrame {
     }
 
     public void configurarComponentes() {
+        random = new Random();
         panelPrincipal = new PanelMinas();
         labelClint     = new JLabel(clint_Idle);
         labelEliot     = new JLabel(eliot_Idle);
@@ -64,7 +67,7 @@ public class InterfazEmpleados extends JFrame {
         panelPrincipal.setLayout(null);
 
         labelClint.setBounds(150, 250, 250, 310);
-        labelEliot.setBounds(200, 280, 110, 250);
+        labelEliot.setBounds(850, 320, 110, 250);
         btnSupervisor.setBounds(600, 600, 100, 30);
 
         panelPrincipal.add(labelClint);
@@ -77,10 +80,23 @@ public class InterfazEmpleados extends JFrame {
         btnSupervisor.addActionListener(e -> {
 
             if (TrabajoEmpleados.isSupervisorHere) {
-                labelClint.setIcon(clint_Work1);
+                int clintAction = random.nextInt(2);
+                if (clintAction == 0) {
+                    labelClint.setIcon(clint_Work1);
+                } else {
+                    labelClint.setIcon(clint_Work2);
+                }
+
+                int eliotAction = random.nextInt(2);
+                if (eliotAction == 0) {
+                    labelEliot.setIcon(eliot_Work1);
+                } else {
+                    labelEliot.setIcon(eliot_Work2);
+                }
 
             } else {
                 labelClint.setIcon(clint_Idle);
+                labelEliot.setIcon(eliot_Idle);
             }
 
             TrabajoEmpleados.isSupervisorHere = !TrabajoEmpleados.isSupervisorHere;
