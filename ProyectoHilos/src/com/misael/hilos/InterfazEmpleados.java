a940b40faa7b583bd8cc6bbedb21a0e6f1160143
misael.alanis <ahangel256@gmail.com>
2022-05-03 22:49:39 -0500

    Progreso empleados

--- ProyectoHilos/src/com/misael/hilos/InterfazEmpleados.java
+++ ProyectoHilos/src/com/misael/hilos/InterfazEmpleados.java
@@ -3,6 +3,8 @@ package com.misael.hilos;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,14 +15,25 @@ public class InterfazEmpleados extends JFrame {
     private PanelMinas panelPrincipal;
     private JLabel     labelClint;
     private JLabel     labelEliot;
-    private JButton    btnSupervisor;
-    private ImageIcon  clint_Idle;
-    private ImageIcon  clint_Work1;
-    private ImageIcon  clint_Work2;
-    private ImageIcon  eliot_Idle;
-    private ImageIcon  eliot_Work1;
-    private ImageIcon  eliot_Work2;
-    private Random     random;
+    private JLabel     labelRobin;
+    private JLabel     btnSupervisor;
+
+    private ImageIcon boton_Supervisar;
+
+    private ImageIcon clint_Idle;
+    private ImageIcon clint_Work1;
+    private ImageIcon clint_Work2;
+
+    private ImageIcon eliot_Idle;
+    private ImageIcon eliot_Work1;
+    private ImageIcon eliot_Work2;
+
+    private ImageIcon robin_Idle;
+    private ImageIcon robin_Work;
+    private ImageIcon robin_Scared;
+
+
+    private Random random;
 
     public InterfazEmpleados() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -28,29 +41,48 @@ public class InterfazEmpleados extends JFrame {
         importarImagenes();
         configurarComponentes();
         initActionListeners();
+        initThreads();
         this.setContentPane(panelPrincipal);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
+    public void initThreads() {
+        TrabajoEmpleados robin = new TrabajoEmpleados("Robin");
+        TrabajoEmpleados clint = new TrabajoEmpleados("Clint");
+        TrabajoEmpleados eliot = new TrabajoEmpleados("Eliot");
+    }
+
     public void importarImagenes() {
         try {
-            BufferedImage ClintIdle_Image  = ImageIO.read(new File("ProyectoHilos/src/resources/clintIdle.png"));
-            Image         ClintWork1_Image = new ImageIcon("ProyectoHilos/src/resources/clintowork1.gif").getImage();
-            Image         ClintWork2_Image = new ImageIcon("ProyectoHilos/src/resources/clintwork2.gif").getImage();
+            BufferedImage boton_Image = ImageIO.read(new File("ProyectoHilos/src/resources/botonSupervisar.png"));
 
-            BufferedImage EliotIdle_Image  = ImageIO.read(new File("ProyectoHilos/src/resources/eliot_idle.png"));
-            Image         EliotWork1_Image = new ImageIcon("ProyectoHilos/src/resources/eliott_work.gif").getImage();
-            Image         EliotWork2_Image = new ImageIcon("ProyectoHilos/src/resources/eliot_work2.gif").getImage();
+            BufferedImage clintIdle_Image  = ImageIO.read(new File("ProyectoHilos/src/resources/clintIdle.png"));
+            Image         clintWork1_Image = new ImageIcon("ProyectoHilos/src/resources/clintowork1.gif").getImage();
+            Image         clintWork2_Image = new ImageIcon("ProyectoHilos/src/resources/clintwork2.gif").getImage();
 
-            clint_Idle  = new ImageIcon(ClintIdle_Image);
-            clint_Work1 = new ImageIcon(ClintWork1_Image);
-            clint_Work2 = new ImageIcon(ClintWork2_Image);
+            BufferedImage eliotIdle_Image  = ImageIO.read(new File("ProyectoHilos/src/resources/eliot_idle.png"));
+            Image         eliotWork1_Image = new ImageIcon("ProyectoHilos/src/resources/eliott_work.gif").getImage();
+            Image         eliotWork2_Image = new ImageIcon("ProyectoHilos/src/resources/eliot_work2.gif").getImage();
 
-            eliot_Idle  = new ImageIcon(EliotIdle_Image);
-            eliot_Work1 = new ImageIcon(EliotWork1_Image);
-            eliot_Work2 = new ImageIcon(EliotWork2_Image);
+            Image robinScared_Image = new ImageIcon("ProyectoHilos/src/resources/robin_scared.gif").getImage();
+            Image robinIdle_Image   = new ImageIcon("ProyectoHilos/src/resources/robin_idle.gif").getImage();
+            Image robinWork_Image   = new ImageIcon("ProyectoHilos/src/resources/robin_work.gif").getImage();
+
+            boton_Supervisar = new ImageIcon(boton_Image);
+
+            clint_Idle  = new ImageIcon(clintIdle_Image);
+            clint_Work1 = new ImageIcon(clintWork1_Image);
+            clint_Work2 = new ImageIcon(clintWork2_Image);
+
+            eliot_Idle  = new ImageIcon(eliotIdle_Image);
+            eliot_Work1 = new ImageIcon(eliotWork1_Image);
+            eliot_Work2 = new ImageIcon(eliotWork2_Image);
+
+            robin_Idle   = new ImageIcon(robinIdle_Image);
+            robin_Scared = new ImageIcon(robinScared_Image);
+            robin_Work   = new ImageIcon(robinWork_Image);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,48 +90,60 @@ public class InterfazEmpleados extends JFrame {
     }
 
     public void configurarComponentes() {
-        random = new Random();
+        random         = new Random();
         panelPrincipal = new PanelMinas();
         labelClint     = new JLabel(clint_Idle);
         labelEliot     = new JLabel(eliot_Idle);
-        btnSupervisor  = new JButton("Supervisar");
+        labelRobin     = new JLabel(robin_Idle);
+        btnSupervisor  = new JLabel(boton_Supervisar);
 
         panelPrincipal.setLayout(null);
 
         labelClint.setBounds(150, 250, 250, 310);
-        labelEliot.setBounds(850, 320, 110, 250);
-        btnSupervisor.setBounds(600, 600, 100, 30);
+        labelRobin.setBounds(900, 280, 150, 250);
+        labelEliot.setBounds(500, 350, 110, 250);
+        btnSupervisor.setBounds(500, 20, 260, 80);
 
         panelPrincipal.add(labelClint);
         panelPrincipal.add(labelEliot);
+        panelPrincipal.add(labelRobin);
         panelPrincipal.add(btnSupervisor);
 
     }
 
     public void initActionListeners() {
-        btnSupervisor.addActionListener(e -> {
+        btnSupervisor.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (TrabajoEmpleados.isSupervisorHere) {
 
-            if (TrabajoEmpleados.isSupervisorHere) {
-                int clintAction = random.nextInt(2);
-                if (clintAction == 0) {
-                    labelClint.setIcon(clint_Work1);
+                    labelRobin.setIcon(robin_Scared);
+                    labelRobin.setIcon(robin_Work);
+
+
+                    int clintAction = random.nextInt(2);
+                    if (clintAction == 0) {
+                        labelClint.setIcon(clint_Work1);
+                    } else {
+                        labelClint.setIcon(clint_Work2);
+                    }
+
+                    int eliotAction = random.nextInt(2);
+                    if (eliotAction == 0) {
+                        labelEliot.setIcon(eliot_Work1);
+                    } else {
+                        labelEliot.setIcon(eliot_Work2);
+                    }
+
                 } else {
-                    labelClint.setIcon(clint_Work2);
+                    labelClint.setIcon(clint_Idle);
+                    labelEliot.setIcon(eliot_Idle);
+                    labelRobin.setIcon(robin_Idle);
                 }
 
-                int eliotAction = random.nextInt(2);
-                if (eliotAction == 0) {
-                    labelEliot.setIcon(eliot_Work1);
-                } else {
-                    labelEliot.setIcon(eliot_Work2);
-                }
+                TrabajoEmpleados.isSupervisorHere = !TrabajoEmpleados.isSupervisorHere;
 
-            } else {
-                labelClint.setIcon(clint_Idle);
-                labelEliot.setIcon(eliot_Idle);
             }
-
-            TrabajoEmpleados.isSupervisorHere = !TrabajoEmpleados.isSupervisorHere;
         });
     }
 
--- ProyectoHilos/src/com/misael/hilos/TrabajoEmpleados.java
+++ ProyectoHilos/src/com/misael/hilos/TrabajoEmpleados.java
@@ -1,13 +1,17 @@
 package com.misael.hilos;
 
+import javax.swing.*;
+
 public class TrabajoEmpleados implements Runnable {
 
     String nombreEmpleado;
     Thread hilo;
+    JLabel aspecto;
+
     static boolean isSupervisorHere;
 
-    public TrabajoEmpleados (String nombreEmpleado) {
-        hilo = new Thread(this, nombreEmpleado);
+    public TrabajoEmpleados(String nombreEmpleado) {
+        hilo                = new Thread(this, nombreEmpleado);
         this.nombreEmpleado = nombreEmpleado;
         hilo.start();
     }
@@ -15,7 +19,7 @@ public class TrabajoEmpleados implements Runnable {
     @Override
     public void run() {
 
-        while (true){
+        while (true) {
 
             try {
                 Thread.sleep(1000);
@@ -23,7 +27,7 @@ public class TrabajoEmpleados implements Runnable {
                 e.printStackTrace();
             }
 
-            if(isSupervisorHere){
+            if (isSupervisorHere) {
                 System.out.println("El supervisor está aquí");
             } else {
                 System.out.println("El supervisor no está aquí");
