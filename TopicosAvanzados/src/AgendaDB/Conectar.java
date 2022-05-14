532763e4e4f2155ef1c6ab70e409cd3590950894
misael.alanis <ahangel256@gmail.com>
2022-05-14 11:22:01 -0500

    Inicio BD Agenda

---
+++ TopicosAvanzados/src/AgendaDB/Conectar.java
@@ -0,0 +1,25 @@
+package AgendaDB;
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectar {
+
+    Connection connection;
+
+    public Connection conexion() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Tipo de conexión, servidor, base de datos, usuario, contraseña
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/trabajo", "root", "");
+            System.out.println("Conexión realizada.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Problemas al conectar");
+        }
+        return connection;
+    }
+
+
+}
