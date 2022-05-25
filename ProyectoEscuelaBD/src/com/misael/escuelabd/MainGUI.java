8098627527288616df4cb1603b0d50d2e080c16f
misael.alanis <ahangel256@gmail.com>
2022-05-24 20:16:32 -0500

    Avance panel tutores

--- ProyectoEscuelaBD/src/com/misael/escuelabd/MainGUI.java
+++ ProyectoEscuelaBD/src/com/misael/escuelabd/MainGUI.java
@@ -3,7 +3,8 @@ package com.misael.escuelabd;
 import com.formdev.flatlaf.FlatLightLaf;
 
 import javax.swing.*;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainGUI extends JFrame {
     private JPanel      panelMain;
@@ -26,9 +27,16 @@ public class MainGUI extends JFrame {
     private JScrollPane spTableAlumnos;
     private JLabel      labelMenuPrincipal;
     private JButton     btnModificarAlumno;
-    private JButton     btnEliminarAlumno;
+    private JButton     btnBajaAlumno;
+    private JTable      tableTutores;
+    private JTextField  textField1;
+    private JButton     btnNuevoTutor;
+    private JButton     btnModificarTutor;
+    private JLabel      labelTituloTutores;
+    private JScrollPane spTutores;
 
     Conectar conectar;
+
 
     public MainGUI() {
         conectar = new Conectar();
@@ -42,11 +50,40 @@ public class MainGUI extends JFrame {
     }
 
     public void initActionListeners() {
+
+        labelAlumnos.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cargarPanelAlumno();
+            }
+        });
+
+        labelTutores.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cargarPanelTutores();
+            }
+        });
+
+        labelGrupos.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cargarPanelGrupos();
+            }
+        });
+
+        labelProfesores.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                cargarPanelProfesores();
+            }
+        });
+
         btnNewAlumno.addActionListener(e -> {
             var newAlumno = new NewAlumnoGUI(this);
         });
 
-        btnEliminarAlumno.addActionListener(e -> {
+        btnBajaAlumno.addActionListener(e -> {
             int idAlumno = (int) tableAlumnos.getValueAt(tableAlumnos.getSelectedRow(), 0);
             int opcion   = JOptionPane.showConfirmDialog(null, "¿Está seguro que desea eliminar este alumno?", "Confirmar", JOptionPane.YES_NO_OPTION);
 
@@ -64,6 +101,34 @@ public class MainGUI extends JFrame {
 
     public void refreshTable() {
         conectar.fillTable("SELECT id_alumno, matricula, nombre, genero, fecha_nacimiento, telefono from alumno");
+    }
+
+    private void cargarPanelAlumno() {
+        panelInformacion.removeAll();
+        panelInformacion.add(panelAlumnos);
+        panelInformacion.revalidate();
+        panelInformacion.repaint();
+    }
+
+    private void cargarPanelTutores() {
+        panelInformacion.removeAll();
+        panelInformacion.add(panelTutores);
+        panelInformacion.revalidate();
+        panelInformacion.repaint();
+    }
+
+    private void cargarPanelGrupos() {
+        panelInformacion.removeAll();
+        panelInformacion.add(panelGrupos);
+        panelInformacion.revalidate();
+        panelInformacion.repaint();
+    }
+
+    private void cargarPanelProfesores() {
+        panelInformacion.removeAll();
+        panelInformacion.add(panelprofesores);
+        panelInformacion.revalidate();
+        panelInformacion.repaint();
     }
 
     public static void main(String[] args) {
