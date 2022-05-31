ac1f602619eeb03aa2ce38af5564a16c137629d1
misael.alanis <ahangel256@gmail.com>
2022-05-30 20:09:33 -0500

    Métodos terminados

--- SimulacionPseudoaleatorios/src/com/misael/simulacion/Launcher.java
+++ SimulacionPseudoaleatorios/src/com/misael/simulacion/Launcher.java
@@ -1,14 +1,18 @@
 package com.misael.simulacion;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 
 public class Launcher {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Metodos.centrosCuadradoDosDigitos(83, 6)));
-        System.out.println(Arrays.toString(Metodos.centrosCuadradoCuatroDigitos(5735, 6)));
-        System.out.println(Arrays.toString(Metodos.fraccionario(0.528, 13)));
-        System.out.println(Arrays.toString(Metodos.productosMedios(5015, 5734, 5)));
+//        System.out.println(Arrays.toString(Metodos.centrosCuadradoDosDigitos(83, 6)));
+//        System.out.println(Arrays.toString(Metodos.centrosCuadradoCuatroDigitos(5735, 6)));
+//        System.out.println(Arrays.toString(Metodos.fraccionario(0.528, 13)));
+//        System.out.println(Arrays.toString(Metodos.productosMedios(5015, 5734, 5)));
+//        System.out.println(Arrays.toString(Metodos.multiplicadorConstante(9803, 6965, 5)));
+        //System.out.println(Arrays.toString(Metodos.congruencialAditivo(65, 89, 98, 3, 69, 100, 7)));
+        System.out.println(Arrays.toString(Metodos.congruencialMixto(37, 19, 33, 100, 4)));
     }
 
 }
--- SimulacionPseudoaleatorios/src/com/misael/simulacion/Metodos.java
+++ SimulacionPseudoaleatorios/src/com/misael/simulacion/Metodos.java
@@ -103,15 +103,73 @@ public class Metodos {
 
     }
 
-    public static void multiplicadorConstante() {
+    public static float[] multiplicadorConstante(int x0, int multiplicador, int n) {
+        float[] respuestasEnteras = new float[n + 1];
+        float[] respuestasFloat   = new float[n + 1];
+        int     siguienteValor;
+        String  valorToString;
+        int     length;
+        respuestasEnteras[0] = x0;
+        respuestasFloat[0]   = respuestasEnteras[0] / 10000;
+
+        for (int i = 1; i < respuestasEnteras.length; i++) {
+            siguienteValor = (int) (respuestasEnteras[i - 1] * multiplicador);
+            valorToString  = String.valueOf(siguienteValor);
+
+            if (valorToString.length() % 2 == 1) {
+                valorToString = "0" + valorToString;
+            }
+
+            length = valorToString.length();
+
+            respuestasEnteras[i] = Integer.parseInt(valorToString.charAt((length / 2) - 2) + "" + valorToString.charAt((length / 2) - 1) + "" + valorToString.charAt((length / 2)) + "" + valorToString.charAt((length / 2) + 1));
+            respuestasFloat[i]   = respuestasEnteras[i] / 10000;
+        }
+        return respuestasFloat;
 
     }
 
-    public static void congruencialMixto() {
+    public static float[] congruencialMixto(int x0, int a, int c, int m, int n) {
+        float[] respuestasEnteras = new float[n + 1];
+        float[] respuestasFloat   = new float[n + 1];
+        int     siguienteValor;
+
+        respuestasEnteras[0] = x0;
+        respuestasFloat[0]   = respuestasEnteras[0] / (m - 1);
+
+
+        for (int i = 1; i < respuestasEnteras.length; i++) {
+            siguienteValor       = (int) (((a * respuestasEnteras[i - 1]) + c) % m);
+            System.out.println(siguienteValor);
+            respuestasEnteras[i] = siguienteValor;
+            respuestasFloat[i]   = respuestasEnteras[i] / (m - 1);
+        }
+        return respuestasFloat;
 
     }
 
-    public static void congruencialAditivo() {
+    public static float[] congruencialAditivo(int x0, int x1, int x2, int x3, int x4, int m, int n) {
+        float[] respuestasEnteras = new float[n + 1];
+        float[] respuestasFloat   = new float[n + 1];
+        int     siguienteValor;
+
+        respuestasEnteras[0] = x0;
+        respuestasEnteras[1] = x1;
+        respuestasEnteras[2] = x2;
+        respuestasEnteras[3] = x3;
+        respuestasEnteras[4] = x4;
+        respuestasFloat[0]   = respuestasEnteras[0] / (m - 1);
+        respuestasFloat[1]   = respuestasEnteras[1] / (m - 1);
+        respuestasFloat[2]   = respuestasEnteras[2] / (m - 1);
+        respuestasFloat[3]   = respuestasEnteras[3] / (m - 1);
+        respuestasFloat[4]   = respuestasEnteras[4] / (m - 1);
+
+        for (int i = 5; i < respuestasEnteras.length; i++) {
+            siguienteValor       = (int) ((respuestasEnteras[i - 1] + respuestasEnteras[i - 5])) % (m - 1);
+            respuestasEnteras[i] = siguienteValor;
+            respuestasFloat[i]   = respuestasEnteras[i] / (m - 1);
+        }
+        return respuestasFloat;
 
     }
 }
